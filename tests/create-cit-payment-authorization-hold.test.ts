08274b7ebd879aca8750915e4ddf961d2003ffec
kotivuori <konsta.kotivuori@paytrail.com>
2024-08-29 16:10:54 +0300

    Fix tests

--- tests/create-cit-payment-authorization-hold.test.ts
+++ tests/create-cit-payment-authorization-hold.test.ts
@@ -71,7 +71,7 @@ describe('create-cit-payment-authorization-hold', () => {
 
     token = await client
       .createGetTokenRequest({
-        checkoutTokenizationId: '818c478e-5682-46bf-97fd-b9c2b93a3fcd'
+        checkoutTokenizationId: '1d0a51f6-a60c-477b-94e2-403a0ed37199'
       })
       .then((res) => res.data.token)
       .catch(() => '')
